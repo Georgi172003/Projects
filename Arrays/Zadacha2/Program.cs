0a3d789cd470a8c18615817d5919d369248b1ea6
Georgi172003 <g.yordanov2017@pmg-pernik.com>
2021-09-29 08:28:43 +0300

    Add Project

---
+++ Arrays/Zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                nums[i] = num;
+            }
+            Console.WriteLine(string.Join(" ", nums.Reverse()));
+        }
+    }
+}
